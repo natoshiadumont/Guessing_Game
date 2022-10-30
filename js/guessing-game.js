0a8a6212d9d67086ce2704ed4386024bf77a65f2
Natoshia Dumont <natoshia.dumont@gmail.com>
2022-10-29 22:45:12 -0500

    submit event listener start

--- js/guessing-game.js
+++ js/guessing-game.js
@@ -8,13 +8,6 @@ a user clicks a button or adds a guess to the input field.
 
 */
 
-//create random number generator and variable to store it
-function randomNum(){
-  return Math.floor(Math.random(100) *100)
- }
-
-
-
 /*make following variables:
 answer
 guess
@@ -23,23 +16,60 @@ tryAgain
 hintButton
 hintText
 */
-
  const answer = randomNum();
- let submit = document.getElementById('submit');
- let guessInput = document.getElementById('guess-input');
- let guessButton = document.getElementById('crystal-ball');
+ let submitGuess = document.getElementById('crystal-ball');
  let restartButton = document.getElementById('restart');
  let hintButton = document.getElementById('hint');
- let guessArray = [... document.querySelector(".block").children];
+let blockCounter=0;
+let chancesRemaining=6;
+let hintText = document.getElementById('hint-text');
+//create event listener that inputs guess in box
+
+submitGuess.addEventListener('click', ()=>{
+  chancesRemaining--;
+  blockCounter++;
+  function pluralS(num){
+    if(num === 0 || num > 1){
+      return 's';
+    }
+  }
+  let guessInput = Number(document.getElementById('guess-input').value);
+  let hintTitle = document.getElementById('hint-text');
+  let hintMessage= document.getElementById('hint-message');
+  hintMessage.innerHTML = 'Your guess must be a number between 1 - 100.';
+  let guessHistory = document.getElementById(`guess${blockCounter}`);
+  if(chancesRemaining < 1){
+    hintTitle.innerHTML = 'Sorry, you are officially out of guesses.';
+    hintMessage.innerHTML = 'Wanna Play Again? Click the restart Button.'
+    return;
+  }
+  if(guessInput >= 1 && guessInput <= 100){
+    guessHistory.innerHTML = guessInput;
+    if(guessInput === answer){
+      hintTitle.innerHTML = 'Congratulations! You guessed the correct number!'
+      hintMessage.innerHTML = 'Wanna Play Again? Click the restart Button.'
+      return;
+    }
+  }
+  else{
+    hintMessage.innerHTML = 'Your guess must be a number between 1 - 100.';
+
+  }
+  console.log(guessInput);
+})
+
+
+
+
+//create random number generator and variable to store it
+function randomNum(){
+  return Math.floor(Math.random(100) *100)
+ }
+
 //create event listenter that refreshes page once you press the reset button
 
 restartButton.addEventListener('click', ()=>{
-  location.reload(true);
-  console.log('working')
+  location.reload();
+  blockCounter = 0;
+  chancesRemaining = 5;
 });
-
-//event listener pushes value of guess into each guess block
-guessButton.addEventListener('click', ()=>{
-
-})
-
