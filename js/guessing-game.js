f62a8d8218de98f4416b65308c3127284f92d902
Natoshia Dumont <natoshia.dumont@gmail.com>
2022-10-30 16:54:13 -0500

    guessing-game: ready for submission

--- js/guessing-game.js
+++ js/guessing-game.js
@@ -16,15 +16,16 @@ tryAgain
 hintButton
 hintText
 */
- const answer = randomNum();
- let submitGuess = document.getElementById('crystal-ball');
- let restartButton = document.getElementById('restart');
- let hintButton = document.getElementById('hint');
+const answer = randomNum();
+let submitGuess = document.getElementById('crystal-ball');
+let restartButton = document.getElementById('restart');
+let hintButton = document.getElementById('hint');
 let blockCounter=1;
 let chancesRemaining=5;
 let hintText = document.getElementById('hint-text');
 //create event listener that inputs guess in box
 console.log('answer:', answer);
+
 submitGuess.addEventListener('click', function inputGuess(){
   
   let guessInput = Number(document.getElementById('guess-input').value);
@@ -32,27 +33,33 @@ submitGuess.addEventListener('click', function inputGuess(){
   let hintMessage= document.getElementById('hint-message');
   hintMessage.innerHTML = '';
   let guessHistory = document.getElementById(`guess${blockCounter}`);
-  if(chancesRemaining < 1){
+  let playerWon = guessHistory === answer;
+  if(playerWon || hintTitle.innerHTML === `Congratulations! You guessed the correct number: ${answer}!` ){
+    hintTitle.innerHTML = `Congratulations! You guessed the correct number: ${answer}!`
+    hintMessage.innerHTML = 'Wanna Play Again? Click the restart Button.'
+   document.getElementById(submitGuess).removeAttribute('disable');
+    return;
+  }
+  
+  else if(chancesRemaining < 1 && !playerWon){
     hintTitle.innerHTML = 'Sorry, you are officially out of guesses.';
     hintMessage.innerHTML = 'Wanna Play Again? Click the restart Button.'
     return;
   }
-
   else if(guessInput >= 1 && guessInput <= 100 && guessInput !== null){
       if(guessInput === answer){
-        chancesRemaining--;
-        blockCounter++;
       guessHistory.innerHTML = guessInput;
       console.log(guessHistory.innerHTML)
       hintTitle.innerHTML = `Congratulations! You guessed the correct number: ${answer}!`
       hintMessage.innerHTML = 'Wanna Play Again? Click the restart Button.'
       return;
-    }
-    chancesRemaining--;
-    blockCounter++;
-    function inputGuess(){guessHistory.innerHTML = guessInput};
-    inputGuess();
-
+      }
+      else{
+        chancesRemaining--;
+        blockCounter++;
+        function inputGuess(){guessHistory.innerHTML = guessInput};
+      inputGuess();
+      }
   }
   else{
     hintMessage.innerHTML = 'Your guess must be a number between 1 - 100.';
@@ -60,34 +67,33 @@ submitGuess.addEventListener('click', function inputGuess(){
 
 
 })
-  
+
 hintButton.addEventListener('click', ()=>{
   let hintMessage= document.getElementById('hint-message');
-  let lastGuess = Number(document.getElementById('guess-input').value);
   if(chancesRemaining >= 5){
     hintMessage.innerHTML = 'HINT: THE ANSWER IS A NUMBER BETWEEN 1 - 100';
+    return;
   }
-  else if(lastGuess >= 1 && lastGuess <= 100){
-    if(lastGuess <= answer + 10 && lastGuess > answer){
-      hintMessage.innerHTML = 'HINT: You are super close! Try guessing a little lower!';
-    }
-    else if(lastGuess >= answer - 10 && lastGuess < answer){
-      hintMessage.innerHTML = 'HINT: You are super close! Try guessing a little higher!';
-    }
-    else if(lastGuess <= answer + 20 && lastGuess > answer){
-      hintMessage.innerHTML = 'HINT: You are getting closer! Try guessing lower!';
-    }
-    else if(lastGuess >= answer - 20 && lastGuess < answer){
-      hintMessage.innerHTML = 'HINT: You are getting closer! Try guessing higher!';
-    }
-    else if(lastGuess < answer){
-      hintMessage.innerHTML = 'HINT: You are not close at all. Guess higher!';
-    }
-    else if(lastGuess > answer){
-      hintMessage.innerHTML = 'HINT: You are not close at all. Guess lower!';
-    }
+  let lastGuess = Number(document.getElementById(`guess${blockCounter - 1}`).innerHTML);
+  
+  if(lastGuess <= answer + 10 && lastGuess > answer){
+    hintMessage.innerHTML = 'HINT: You are super close! Try guessing a little lower!';
   }
-
+  else if(lastGuess >= answer - 10 && lastGuess < answer){
+    hintMessage.innerHTML = 'HINT: You are super close! Try guessing a little higher!';
+  }
+  else if(lastGuess <= answer + 20 && lastGuess > answer){
+    hintMessage.innerHTML = 'HINT: You are getting closer! Try guessing lower!';
+  }
+  else if(lastGuess >= answer - 20 && lastGuess < answer){
+    hintMessage.innerHTML = 'HINT: You are getting closer! Try guessing higher!';
+  }
+  else if(lastGuess < answer){
+    hintMessage.innerHTML = 'HINT: You are not close at all. Guess higher!';
+  }
+  else if(lastGuess > answer){
+    hintMessage.innerHTML = 'HINT: You are not close at all. Guess lower!';
+  }
 })
 
 
